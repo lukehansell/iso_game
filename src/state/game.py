3e3c8e5715d771c8ffdfcf0c8154b268d23e5c2e
Luke Hansell <lukehansell@gmail.com>
2021-01-18 12:28:26 +0000

    ticking month with game

--- src/state/game.py
+++ src/state/game.py
@@ -14,6 +14,16 @@ def reducer(state, action):
         profit = reduce(calc_profit, action.tiles, 0)
         new_state = state.copy()
         new_state['balance'] = state['balance'] + profit
+
+        new_month = state['date']['month'] + 1
+        new_year = state['date']['year']
+        if new_month >= 12:
+            new_month = 0
+            new_year += 1
+
+        new_state['date']['month'] = new_month
+        new_state['date']['year'] = new_year
+
         return new_state
 
     return state
